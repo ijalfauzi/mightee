198afa1712fec66d084d30ad097fa5458cf858be
Ijal Fauzi <ijaliffauzi@gmail.com>
2023-07-14 15:46:12 +0700

    Updated indentation

--- 404.php
+++ 404.php
@@ -3,14 +3,13 @@
 <div id="main-container" class="main-container">
     <div class="container">
         <div class="row">
-        <h1>
-    <?php esc_html_e( '404', 'mightee' ); ?>
-</h1>
-<p>
-    <?php esc_html_e( 'Sorry, the page that you are looking for was not found at the same time. Please use the search form below', 'mightee'); ?>
-</p>
-
-<?php get_search_form(); ?>
+            <h1>
+                <?php esc_html_e( '404', 'mightee' ); ?>
+            </h1>
+            <p>
+                <?php esc_html_e( 'Sorry, the page that you are looking for was not found at the same time. Please use the search form below', 'mightee'); ?>
+            </p>
+            <?php get_search_form(); ?>
         </div>
     </div>
 </div>
