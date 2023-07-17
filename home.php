f7eb766a20f3445151133bb91d4bfaf97ea0780b
Ijal Fauzi <ijaliffauzi@gmail.com>
2023-07-17 16:16:36 +0700

    Updated blog index with excerpt

--- home.php
+++ home.php
@@ -1,28 +1,29 @@
 <?php get_header(); ?>
 
-<div id="main-container" class="main-container">
+<section id="main-container" class="main-container">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 mb-5 mb-lg-0">
                 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                     <div class="post">
                         <div class="post-media post-image">
-
+                            <?php if ( has_post_thumbnail()) : ?>
+                                <a href="<?php the_permalink(); ?>" alt="<?php the_title_attribute(); ?>">
+                                    <?php the_post_thumbnail(); ?>
+                                </a>
+                            <?php endif; ?>
                         </div>
                         <div class="post-body">
                             <div class="entry-header">
                                 <div class="post-meta">
                                     <span class="post-author">
-                                        <i class="far fa-user"></i> <?php the_author(); ?>
+                                        <i class="far fa-user"></i> <?php the_author_posts_link(); ?>
                                     </span>
                                     <span class="post-cat">
-                                        <i class="far fa-folder-open"></i> <?php 
-                                        $category = get_the_category();
-                                        echo $category[0]->cat_name;
-                                        ?>
+                                        <i class="far fa-folder-open"></i> <?php echo get_the_category_list(', '); ?>
                                     </span>
                                     <span class="post-meta-date">
-                                        <i class="far fa-calendar"></i> <?php the_time( 'F j, Y'); ?>
+                                        <i class="far fa-calendar"></i> <?php the_time( 'F jS, Y'); ?>
                                     </span>
                                     <span class="post-comment">
                                         <i class="far fa-comment"></i> <?php comments_number(); ?>
@@ -33,10 +34,10 @@
                                 </h2>
                             </div>
                             <div class="entry-content">
-                                <?php the_content(); ?>
-                            </div>
-                            <div class="post-footer">
-                                <a href="" class="btn btn-primary">Continue Reading</a>
+                                <?php the_excerpt(); ?>
+                                <p>
+                                    <a class="btn btn-primary" href="<?php the_permalink(); ?>">Continue Reading</a>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -54,6 +55,6 @@
             </div>
         </div>
     </div>
-</div>
+</section>
 
 <?php get_footer(); ?>
